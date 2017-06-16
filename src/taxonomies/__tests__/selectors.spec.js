0337a4a14e0fa94f4537a7283c19e81699c43d5f
andrewmclagan <andrew@ethicaljobs.com.au>
2017-06-16 15:13:59 +1000

    Ordered Taxonomy selector

--- src/taxonomies/__tests__/selectors.spec.js
+++ src/taxonomies/__tests__/selectors.spec.js
@@ -21,4 +21,67 @@ test('taxonomiesSelector returns correct state slice', () => {
   return expect(Immutable.is('foo-bar-bam', selectors.taxonomiesSelector(state))).toBe(true);
 });
 
+test('orderedTaxonomySelector returns correct state slice', () => {
+  const state = Immutable.fromJS({
+    entities: {
+      taxonomies: {
+        taxonomies: {
+          "categories": {
+            "1": {
+              "id": 1,
+              "slug": "zulu",
+              "title": "zulu"
+            },
+            "2": {
+              "id": 2,
+              "slug": "bravo",
+              "title": "bravo"
+            },
+            "3": {
+              "id": 3,
+              "slug": "alpha",
+              "title": "alpha"
+            },
+            "4": {
+              "id": 4,
+              "slug": "yankee",
+              "title": "yankee"
+            },
+          },
+          "locations": {
+            "1": {
+              "id": 1,
+              "slug": "VIC",
+              "title": "Melbourne"
+            },
+          },
+          "sectors": {
+            "1": {
+              "id": 1,
+              "slug": "Business and Private Sector",
+              "title": "Business/Private Sector"
+            },
+          },
+          "workTypes": {
+            "1": {
+              "id": 1,
+              "slug": "CASUAL",
+              "title": "Casual"
+            },
+          },
+        },
+      },
+    }
+  });
+  const expected = state.getIn(['entities','taxonomies','taxonomies','categories']);
+  const actual = selectors.orderedTaxonomySelector(state, 'categories');
+  expect(Immutable.isOrdered(actual)).toBe(true);
+  const asList = actual.toList();
+  expect(asList.get(0).get('title')).toBe('alpha');
+  expect(asList.get(1).get('title')).toBe('bravo');
+  expect(asList.get(2).get('title')).toBe('yankee');
+  expect(asList.get(3).get('title')).toBe('zulu');
+});
+
+
 
--- src/taxonomies/selectors.js
+++ src/taxonomies/selectors.js
@@ -1,3 +1,4 @@
+import Immutable from 'immutable';
 import { SelectorFactory } from 'ethical-jobs-redux';
 import { createSelector } from 'reselect';
 
@@ -6,3 +7,9 @@ export const fetchingSelector = SelectorFactory.create('taxonomies', 'fetching')
 export const errorSelector = SelectorFactory.create('taxonomies', 'error');
 
 export const taxonomiesSelector = state => state.getIn(['entities', 'taxonomies', 'taxonomies']);
+
+export const orderedTaxonomySelector = (state, taxonomy) => {
+  return taxonomiesSelector(state)
+  .get(taxonomy, Immutable.Map())
+  .sort((a, b) => a.get('title').localeCompare(b.get('title')))
+};
