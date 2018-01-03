69144e0cf6516c0557bb319bc5dd37f422d1a5f4
Sebastian Sibelle <sebastian@ethicaljobs.com.au>
2018-01-03 15:03:49 +1100

    Added selector tests

---
+++ src/subscriptions/__tests__/selectors.spec.js
@@ -0,0 +1,29 @@
+import Immutable from 'immutable';
+import { Assertions } from 'ethical-jobs-redux';
+import Subscriptions from 'subscriptions';
+
+const { selectors } = Subscriptions;
+
+test('fetching returns correct state slice', () => {
+  expect(
+    Assertions.fetchingSelector('subscriptions', selectors.fetching)
+  ).toBe(true);
+});
+
+test('result selector returns correct state slice', () => {
+  expect(
+    Assertions.resultSelector('subscriptions', selectors.result)
+  ).toBe(true);
+});
+
+test('subscriptions selector returns correct state slice', () => {
+  expect(
+    Assertions.entitiesSelector('subscriptions', 'subscriptions', selectors.subscriptions)
+  ).toBe(true);
+});
+
+test('alerts selector returns correct state slice', () => {
+  expect(
+    Assertions.entitiesSelector('subscriptions', 'alerts', selectors.alerts)
+  ).toBe(true);
+});
